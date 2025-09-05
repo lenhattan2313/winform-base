e5cbe4663ac7021271f387fec45353d424dddcb7
Tan <lenhattan2313@gmail.com>
2025-09-05 23:40:58 +0700

    Add weighing system

---
+++ MyDashboard.WPF/Views/Main/WeighingSystem.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace MyDashboard.WPF.Views.Main
+{
+    /// <summary>
+    /// Interaction logic for WeighingSystem.xaml
+    /// </summary>
+    public partial class WeighingSystem : UserControl
+    {
+        public WeighingSystem()
+        {
+            InitializeComponent();
+        }
+    }
+}
